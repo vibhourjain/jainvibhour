530753d54584f87ccfada2eac4466a350c0d5436
vibhourjain <87240979+vibhourjain@users.noreply.github.com>
2024-06-17 11:07:21 +0800

    Update UNIX_os_UPGRADE_BACKUP.sh

--- UNIX_os_UPGRADE_BACKUP.sh
+++ UNIX_os_UPGRADE_BACKUP.sh
@@ -1,4 +1,4 @@
-BASE_FOLDER="/user/OS_UPGRADE/$(hostname)"
+BASE_FOLDER="/user/OS_UPGRADE/"
 
 LOG_FILE =$BASE_FOLDER/$(hostname)_pre_upgrade_task_script.log
 exec > "$LOG_FILE" 2>&1
@@ -13,4 +13,9 @@ fi
 start_time=$(date +%s)
 
 #Create Backup directory
-BACKUP_DIR=
+BACKUP_DIR=$BASE_FOLDER/$(hostname)
+
+
+#list network interfaces
+nic_list=$(ip -o link show | awk -F': ' '{print $2}')
+
